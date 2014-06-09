70220577b6df671f25fc26470b8e5dc70c538801
Natalie <natalie.pollard@richmond.edu>
2014-06-09 13:50:32 -0600

    Wrote the method to find the nodes within n hops

--- datasetGraph.py
+++ datasetGraph.py
@@ -3,6 +3,23 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+#This method performs a recursive depth first search to add all the nodes within a 
+#specified distance of the specified node
+
+#returns a list containing all the nodes within numberOfHops 
+def getReachableInNHops(numberOfHops, currentDepth, listOfRechables, node):
+	if currentDepth == numberOfHops:
+		return listOfRechables
+	
+	for n in subDG.neighbors(node):
+		if not n in listOfRechables:
+			#print "appending %s" % n
+			listOfRechables.append(n)
+			getReachableInNHops(numberOfHops, currentDepth+1, listOfRechables, n)
+
+
+
+
 names = open('advogato/ent.advogato.user.name', 'r')
 edges = open('advogato/out.advogato', 'r')
 
@@ -12,9 +29,11 @@ subGraph = []
 
 for line in names:
 	namesList.append(line)
-
 for i in range(0,50):
 	subGraph.append(namesList[i-1])
+
+
+#subGraph = namesList;
 
 DG = nx.DiGraph()
 masterDG = nx.DiGraph()
@@ -42,31 +61,31 @@ subMasterDG = masterDG.subgraph(subGraph)
 subJourneyerDG = journeyerDG.subgraph(subGraph)
 subApprenticeDG = apprenticeDG.subgraph(subGraph)
 
+plt.figure(figsize=(9,9))
 
 
-plt.figure(figsize=(10,10))
+listOfRechables = []
+node = subDG.nodes()[16]
+numHops = 3
+listOfRechables.append(node)
+print "the start node is %s" %node
+
+#getReachableInNHops(numberOfHops, depth, listOfRechables, node)
+getReachableInNHops(numHops, 0, listOfRechables, node)
+
+reachableWithin3HopsGraph  = subDG.subgraph(listOfRechables)
+#nx.draw(reachableWithin3HopsGraph)
+print "The nodes reachable within %d hops of %s" %(numHops, node)
+print listOfRechables
+print "The length of the list is %d" %len(listOfRechables)
 
 
-listOfNeighbors = []
-#listOfNeighbors = subDG.neighbors(subDG[1])
-"""for i in subDG.neighbors_iter(0)
-	if i == 0:
-		continue
-	listOfNeighbors.append(i)
-
-neighborsGraph = subDG.subgraph(listOfNeighbors)
-nx.draw(neighborsGraph)
-	
-
-
-"""
-
-
-pos=nx.spring_layout(subDG)
+pos=nx.random_layout(subDG)
 nx.draw_networkx_nodes(subDG, pos, node_size = 75)
 nx.draw_networkx_edges(subMasterDG, pos)
 nx.draw_networkx_edges(subJourneyerDG, pos, edge_color = 'g')
 nx.draw_networkx_edges(subApprenticeDG, pos, edge_color = 'b')
+nx.draw_networkx_labels(subDG, pos)
 
 plt.show()
 
